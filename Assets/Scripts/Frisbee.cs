dd4244ed3236ce0d1db46844e8376bfc732f8006
sikny <pamartqu@gmail.com>
2020-09-28 10:33:05 +0200

    frisbee script, accepts direction & force

---
+++ Assets/Scripts/Frisbee.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Frisbee : MonoBehaviour {
+    private Vector2 _direction;
+    private float _force;
+    
+    private void Update() {
+        Vector3 pos = transform.position;
+        Vector3 dest = pos;
+        dest.x += _direction.x;
+        dest.z += _direction.y;
+        transform.position = Vector3.MoveTowards(pos, dest, _force * Time.deltaTime);
+    }
+
+    public void Move(Vector2 direction, float force) {
+        _direction = direction.normalized;
+        _force = force;
+    }
+}
