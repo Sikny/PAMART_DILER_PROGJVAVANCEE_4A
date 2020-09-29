1cd1f403020d4df7c36d3b04d14170d90027da59
sikny <pamartqu@gmail.com>
2020-09-29 12:10:15 +0200

    menu pause behaviour

---
+++ Assets/Scripts/GamePauser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePauser : MonoBehaviour
+{
+    public void OnPauseButton() {
+        Time.timeScale = 0f;
+    }
+
+    public void OnResumeButton() {
+        Time.timeScale = 1f;
+    }
+
+    public void OnRestartButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void OnMainMenuButton() {
+        SceneManager.LoadScene("Menu");
+    }
+}
